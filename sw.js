05b4dac3a298bab8dbef8f08d00b511429d6d512
mdfazlerabbi2060-cmd <mdfazlerabbi2060@gmail.com>
2026-06-20 10:08:48 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,3 +4,4 @@ self.options = {
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+<script>(function(s){s.dataset.zone='11175440',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
